b163d93e80fa170d0ff8da7120a71b9338252abd
Mostafa-Babaei <83201012+Mostafa-Babaei@users.noreply.github.com>
2021-12-19 19:43:05 +0330

    افزودن جدول لیست انتظار

---
+++ PrivateTraining.DomainClasses/Entities/PrivateTraining/ServiceWaitingList.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrivateTraining.DomainClasses.Entities.PrivateTraining
+{
+
+    [Table("ServiceWaitingList", Schema = "PrivateTraining")]
+    public class ServiceWaitingList : BaseEntity
+    {
+        public int BuyServiceId { get; set; }
+        public int serviceProviderId { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsPending { get; set; }
+        public bool IsAccept { get; set; }
+        public DateTime CreateAt { get; set; }
+    }
+}
