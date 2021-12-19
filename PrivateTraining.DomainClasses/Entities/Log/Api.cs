1667dabfae0204a756889c4132b66723f2cb8865
Mostafa-Babaei <83201012+Mostafa-Babaei@users.noreply.github.com>
2021-12-19 19:04:50 +0330

    Add Api File

---
+++ PrivateTraining.DomainClasses/Entities/Log/Api.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PrivateTraining.DomainClasses.Entities.Log
+{
+    [Table("Api", Schema = "Log")]
+    public class Api
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long id { get; set; }
+
+        [Display(Name = "text")] public string text { get; set; }
+
+        [Display(Name = "detail")] public string detail { get; set; }
+    }
+}
