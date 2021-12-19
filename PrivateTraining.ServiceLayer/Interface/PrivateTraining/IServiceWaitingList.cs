f3d0a4a5c07eaae0e65858e7cf18b6cfe81d6462
Mostafa-Babaei <83201012+Mostafa-Babaei@users.noreply.github.com>
2021-12-19 20:05:52 +0330

    افزودن سرویس لیست انتظار

---
+++ PrivateTraining.ServiceLayer/Interface/PrivateTraining/IServiceWaitingList.cs
@@ -0,0 +1,48 @@
+﻿using PrivateTraining.DomainClasses.Entities.PrivateTraining;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace PrivateTraining.ServiceLayer.Interface.PrivateTraining
+{
+    public interface IServiceWaitingList
+    {
+        /// <summary>
+        /// دریافت لیست خدمتیاران لیست انتظار سرویس
+        /// </summary>
+        /// <param name="ServiceId"></param>
+        /// <returns></returns>
+        Task<List<ServiceWaitingList>> GetWaitListService(int ServiceId);
+
+        /// <summary>
+        /// تائید درخواست سرویس
+        /// </summary>
+        /// <param name="ServiceId"></param>
+        /// <param name="ProviderId"></param>
+        /// <returns></returns>
+        Task ProviderAcceptRequest(int ServiceId, int ProviderId);
+
+        /// <summary>
+        /// تائید اولیه درخواست توسط خدمتیار جهت مذاکره با مشتری
+        /// </summary>
+        /// <param name="ServiceId"></param>
+        /// <param name="ProviderId"></param>
+        /// <returns></returns>
+        Task ProviderPendRequest(int ServiceId, int ProviderId);
+
+        /// <summary>
+        /// افزودن لیست خدمتیاران به لیست انتظار
+        /// </summary>
+        /// <param name="ServiceId"></param>
+        /// <param name="ProviderId"></param>
+        /// <returns></returns>
+        Task AddProviderToWaitListService(int ServiceId, List<int> ProviderId);
+
+        /// <summary>
+        /// حذف یا غیر فعال کردن لیست خدمتیاران از لیست انتظار
+        /// </summary>
+        /// <param name="ServiceId"></param>
+        /// <param name="ProviderId"></param>
+        /// <returns></returns>
+        Task DeactiveProviderFromWaitListService(int ServiceId, List<int> ProviderId);
+    }
+}
---
+++ PrivateTraining.ServiceLayer/Repository/PrivateTraining/ServiceWaitingList.cs
@@ -0,0 +1,49 @@
+﻿using PrivateTraining.DataLayer.Context;
+using PrivateTraining.DomainClasses.Entities.PrivateTraining;
+using PrivateTraining.ServiceLayer.Interface.PrivateTraining;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrivateTraining.ServiceLayer.Repository.PrivateTraining
+{
+    public class ServiceWaitingList : IServiceWaitingList
+    {
+        private readonly IUnitOfWork _uow;
+        readonly IDbSet<ServiceWaitingList> _Service;
+
+        public ServiceWaitingList(IUnitOfWork uow)
+        {
+            _uow = uow;
+            _Service = _uow.Set<ServiceWaitingList>();
+        }
+
+        public Task AddProviderToWaitListService(int ServiceId, List<int> ProviderId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeactiveProviderFromWaitListService(int ServiceId, List<int> ProviderId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<DomainClasses.Entities.PrivateTraining.ServiceWaitingList>> GetWaitListService(int ServiceId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task ProviderAcceptRequest(int ServiceId, int ProviderId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task ProviderPendRequest(int ServiceId, int ProviderId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
